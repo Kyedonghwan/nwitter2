83f9284e48f0a19db39b5ef6704e38fb0f3c19cc
Kyedonghwan <kdh6437@nts-corp.com>
2023-12-04 16:05:12 +0900

    set Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,57 @@
-import { useState } from 'react'
+import React, { useState } from 'react';
+import { createBrowserRouter } from 'react-router-dom';
+import { createGlobalStyle } from 'styled-components';
+import { RouterProvider } from '../node_modules/react-router-dom/dist/index';
+import reset from 'styled-reset';
+import Layout from './components/layout';
+import CreateAccount from './routes/create-account';
+import Home from './routes/Home';
+import Login from './routes/login';
+import Profile from './routes/Profile';
+
+const router = createBrowserRouter([
+  {
+    path:"/",
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/profile",
+        element: <Profile />,
+      }
+    ]
+  },
+  {
+    path:"/login",
+    element: <Login />,
+  },
+  {
+    path:"/createaccount",
+    element: <CreateAccount />,
+  }
+])
+
+const GlobalStyles = createGlobalStyle`
+  ${reset};
+  body {
+    background-color: black;
+    color: white;
+    font-family: -apple-system
+  }
+`
 
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div>
-      sadasd
-    </div>
+    <>
+      <GlobalStyles />
+      <RouterProvider router={router} />
+    </>
   )
 }
 
---
+++ src/components/layout.tsx
@@ -0,0 +1,10 @@
+import { Outlet } from "../../node_modules/react-router-dom/dist/index";
+
+export default function Layout() {
+  return (
+    <>
+      <h1>Layout</h1>
+      <Outlet />
+    </>
+  )
+}
